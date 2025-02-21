8ab6d2b9100ddbab9d262ad9043e67eb880b6eec
HemChandYesala <hemichand.y@gmail.com>
2025-02-21 18:11:32 +0530

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>Git Hub Actions Project Deploying the Project Check !</h1>
-      <h1>Added another Tag !</h1>
+      <h1>Git Hub Actions Project Deploying the Project Check  this is changes becoz of the Test Cases!</h1>
     </>
   )
 }
